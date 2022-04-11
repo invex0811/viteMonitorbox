defa1de876cebbcfd615a520465d5ba25f8cebba
invex0811 <ighor.kesua37@gmail.com>
2022-04-11 10:10:23 +0300

    images

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
   define: { 'process.env': {} },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@/': `${path.resolve(__dirname, 'src')}/`,
     },
   },
   server: {
