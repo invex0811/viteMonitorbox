2bada0da5fc8ea757f2358a5694b6b72e9f94f15
invex0811 <ighor.kesua37@gmail.com>
2022-05-06 14:17:37 +0300

    add page autocommit

---
+++ assets/lazy.6a4a58d5.js
@@ -0,0 +1,1 @@
+import{h as r,r as n,b as u,D as f}from"./index.5c440f67.js";const c=r({eager:Boolean},"lazy");function m(a,o){const e=n(!1),s=u(()=>e.value||a.eager||o.value);f(o,()=>e.value=!0);function t(){a.eager||(e.value=!1)}return{isBooted:e,hasContent:s,onAfterLeave:t}}export{c as m,m as u};
